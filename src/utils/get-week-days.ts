824a9f3ed73aad119328c6774d100c613239452b
Filipe Dias <filipediascontato@gmail.com>
2023-03-26 20:09:13 -0300

    feat: Creates the Calendar component

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,9 +1,17 @@
-export function getWeekDays() {
+interface GetWeekDaysOptions {
+  short?: boolean
+}
+
+export function getWeekDays(options?: GetWeekDaysOptions) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekDay) => {
+      if (options?.short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
+
       return weekDay.charAt(0).toUpperCase().concat(weekDay.substring(1))
     })
 }
