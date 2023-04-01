3eb5bb287dda9b321dec2622a6f3eedc478bb547
Filipe Dias <filipediascontato@gmail.com>
2023-04-01 19:43:31 -0300

    feat: Adds googleapi integration

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -12,10 +12,13 @@ export function buildNextAuthOptions(
     adapter: PrismaAdapter(req, res),
     providers: [
       GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        clientId: process.env['GOOGLE_CLIENT_ID'] ?? '',
+        clientSecret: process.env['GOOGLE_CLIENT_SECRET'] ?? '',
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },
--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
 import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
-import { ShieldCheckered } from 'phosphor-react'
+import { getGoogleOAuthToken } from '@/lib/googleapi'
 
 const scheduleBodySchema = z.object({
   name: z.string(),
@@ -51,13 +52,42 @@ export default async function handler(
       .json({ message: 'This time has already been scheduled.' })
   }
 
-  await prisma.schedule.create({
+  const schedule = await prisma.schedule.create({
     data: {
       name,
       email,
       notes,
       date: scheduleDateTime,
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: notes,
+      start: {
+        dateTime: scheduleDate.format(),
+      },
+      end: {
+        dateTime: scheduleDate.add(1, 'hour').format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: schedule.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
